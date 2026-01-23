e548048dbe0706c84b1c4adeb4aa0c2df174a2ad
Crutcher Dunnavant <crutcher@gmail.com>
2026-01-22 16:33:14 -0800

    Unroll `use ...::*;` imports. (#56)

--- crates/wordchuck/src/vocab/mod.rs
+++ crates/wordchuck/src/vocab/mod.rs
@@ -8,6 +8,6 @@ pub mod unified_vocab;
 pub mod vocab_index;
 pub mod word_vocab;
 
-pub use unified_vocab::*;
-pub use vocab_index::*;
-pub use word_vocab::*;
+pub use unified_vocab::UnifiedTokenVocab;
+pub use vocab_index::TokenVocabIndex;
+pub use word_vocab::WordMapTokenVocab;
