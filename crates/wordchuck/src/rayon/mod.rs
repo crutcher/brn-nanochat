b49e8b4279fb16b2662b0252cc321a4de43aebe4
Crutcher Dunnavant <crutcher@gmail.com>
2026-01-22 16:32:16 -0800

    Unroll `use ...::*;` imports.

--- crates/wordchuck/src/rayon/mod.rs
+++ crates/wordchuck/src/rayon/mod.rs
@@ -3,5 +3,5 @@
 mod rayon_decoder;
 mod rayon_encoder;
 
-pub use rayon_decoder::*;
-pub use rayon_encoder::*;
+pub use rayon_decoder::ParallelRayonDecoder;
+pub use rayon_encoder::ParallelRayonEncoder;
