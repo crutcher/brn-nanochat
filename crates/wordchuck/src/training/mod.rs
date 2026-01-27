89bc2bebaeca052e0a8b779ab3ccf0ea8d9f6342
Crutcher Dunnavant <crutcher@gmail.com>
2026-01-27 14:45:25 -0800

    Update documentation for encoders, decoders, and training modules; bump version to 0.0.9

--- crates/wordchuck/src/training/mod.rs
+++ crates/wordchuck/src/training/mod.rs
@@ -1,6 +1,34 @@
 //! # Vocabulary Training
 //!
+//! Support for training token vocabularies.
+//!
+//! Training requires:
+//! * [`crate::vocab::ByteMapVocab`] - a choice of ``{ u8 -> T }` byte mappings.
+//!   * The default is ``T::from(u8)``.
+//! * [`crate::segmentation::SegmentationConfig`] - a text splitting config.
+//!   * This can be built from just a regex pattern.
+//!   * Special tokens can be overlaid on a pre-trained vocabulary.
+//!
 //! ## Training Example
+//!
+//! See `examples/tokenizer_trainer`.
+//!
+//! This is a code snippet overview of training.
+//!
+//! Expect training to take ~1s/10MB of input; and to be slowed
+//! primarily by how well the stream logic of loading the training
+//! samples is parallelized.
+//!
+//! Note: currently, training has limited logging and no progress reporting.
+//!
+//! A common training binary is probably a good idea; and much of the messiness
+//! of supporting many different training data sources could be hidden in
+//! the isolated deps of such a tool.
+//!
+//! Here:
+//!
+//! - The iterator stream for samples may be quite large.
+//! - Training a `nanochat` equivalent tokenizer takes ~80 CPU minutes.
 //!
 //! Consider the following, to train a tokenizer and export it a "*.tiktoken" file.
 //!
